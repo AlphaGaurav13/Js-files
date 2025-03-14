98902f7e0ea7c4d684c920f9b419e1bca6d567d8
Gauravkumar <144926875+AlphaGaurav13@users.noreply.github.com>
2025-03-14 04:15:28 +0000

    Type_casting Here!

---
+++ basics/datatype_Convertion.js
@@ -0,0 +1,39 @@
+let num = "33";
+
+// console.log(typeof(num));
+
+//typeconvertion also we called it typecasting.
+let numVal = Number(num);
+
+
+// console.log(typeof numVal);
+
+
+//if you convert something which is not a actual number then if try to print the value of that number it will give you output = NaN.
+// so it is not a strict thats we use typescript.
+
+// "33"  => 33
+// "33abc" => NaN
+// true => 1 and false => 0
+
+// null => 0
+// undefine  => undefine
+
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn); // true 
+
+// 1 => true : 0 =>  false
+// "" => false  : here string is empty
+// "Gaurav" => true
+
+let number = 13
+
+let stringNumber = String(number);
+
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
