148b40700c008da6a9a60fb7222379211a9899e9
Gauravkumar <144926875+AlphaGaurav13@users.noreply.github.com>
2025-03-14 05:16:55 +0000

    Datatype_summary (imp)

---
+++ basics/Datatype_Summary.js
@@ -0,0 +1,44 @@
+// Primitive 
+
+// 7 data Types : String, Number, Boolean, null, undefined,  Symbol, BigInt.
+
+//JavaScript is a dynamcally typed language which means data type is determined by the value at the runtime and can be changed thoughout the program as different value can assgned to them.
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123');
+
+// console.log(id === anotherId);  // false
+
+
+// Reference type (Non Primitive)
+
+//Array , objects , Functions
+
+const hero = ["Gaurav", "Krishna", "Saktiman"];  //array representation
+// we write object in curly brackets {}
+
+let myObj = {
+    name:  "Gaurav",
+    age :  20,
+};
+
+const myFunction = function() {
+    console.log("Hello World");
+}
+let num = 10;
+let str1 = "Gaurav";
+let isTrue = true;
+let hero1 = null;
+// console.log(typeof myFunction); // function
+// console.log(typeof myObj);  // object
+// console.log(typeof num);  // number
+// console.log(typeof str1);  // string
+// console.log(typeof isTrue);  // boolean
+// console.log(typeof hero1);  //  object
+
+
+
+
+
+
