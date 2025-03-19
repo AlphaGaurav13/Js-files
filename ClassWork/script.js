c4ce7f5560689703e0343fa920d22aabec5d2dd8
Gauravkumar <144926875+AlphaGaurav13@users.noreply.github.com>
2025-03-19 09:31:26 +0000

    boi

--- ClassWork/script.js
+++ ClassWork/script.js
@@ -1,37 +1,48 @@
-//3. Promt
+// //3. Promt
 
-let name1 = prompt("Enter the name", "Gaurav Kumar");
-console.log(`Helo   , ${name1} `);
+// let name1 = prompt("Enter the name", "Gaurav Kumar");
+// console.log(`Helo   , ${name1} `);
 
 
-// js objects
+// // js objects
 
-let car = {
-    name: "BMW",
-    model: "X5",
-    color: "black",
-    // start:function() {
-    //     console.log("Car is start");
-    // },
+// let car = {
+//     name: "BMW",
+//     model: "X5",
+//     color: "black",
+//     // start:function() {
+//     //     console.log("Car is start");
+//     // },
 
+// }
+
+// // car.start();
+
+
+// console.log(Object.keys(car));
+
+// console.log(Object.values(car));
+
+
+// // iterate over object using for-in loop
+
+// for(let key in car) {
+//     console.log(`${key} : `, car[key]);
+// }
+
+// //Finding the maximum element inside the array
+
+// console.log(Math.PI.toFixed(2));
+
+// console.log(`Max Value inside the array  is : `,Math.max(12,3,4,5,6));
+
+
+function greet(name) {
+
+    const text = document.querySelector('.ga');
+    text.innerHTML = `hello ${name} Welcome to Our Website`;
+    text.
+    
 }
 
-// car.start();
-
-
-console.log(Object.keys(car));
-
-console.log(Object.values(car));
-
-
-// iterate over object using for-in loop
-
-for(let key in car) {
-    console.log(`${key} : `, car[key]);
-}
-
-//Finding the maximum element inside the array
-
-console.log(Math.PI.toFixed(2));
-
-console.log(`Max Value inside the array  is : `,Math.max(12,3,4,5,6));
+greet("Rishab Aur Rohit :)");
