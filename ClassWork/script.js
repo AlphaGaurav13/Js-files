9a7363237ae2216a5c54c8ca416a1263d4a79330
Gauravkumar <144926875+AlphaGaurav13@users.noreply.github.com>
2025-03-19 09:11:26 +0000

    Class - Work

---
+++ ClassWork/script.js
@@ -0,0 +1,37 @@
+//3. Promt
+
+let name1 = prompt("Enter the name", "Gaurav Kumar");
+console.log(`Helo   , ${name1} `);
+
+
+// js objects
+
+let car = {
+    name: "BMW",
+    model: "X5",
+    color: "black",
+    // start:function() {
+    //     console.log("Car is start");
+    // },
+
+}
+
+// car.start();
+
+
+console.log(Object.keys(car));
+
+console.log(Object.values(car));
+
+
+// iterate over object using for-in loop
+
+for(let key in car) {
+    console.log(`${key} : `, car[key]);
+}
+
+//Finding the maximum element inside the array
+
+console.log(Math.PI.toFixed(2));
+
+console.log(`Max Value inside the array  is : `,Math.max(12,3,4,5,6));
